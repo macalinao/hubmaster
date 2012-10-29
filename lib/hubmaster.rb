3f5b1d5eaec5adc8c77a25185a68f15c2b7baf2b
Tommy Schaefer <me@tommyschaefer.net>
2012-10-29 16:02:06 -0500

    f

--- lib/hubmaster.rb
+++ lib/hubmaster.rb
@@ -1,16 +1,23 @@
 require 'rubygems'
 require 'highline/import'
+require 'json'
 require 'net/http'
 require 'net/https'
 require 'stringio'
 
 module Github
   def self.connect
+    if File.exists? "#{Dir.home}/.hubmaster"
+      file = File.open("#{Dir.home}/.hubmaster", "rb")
+      @user = file.read
+    else
       print "Username: "
       @user = STDIN.gets.chomp
-      @pass = ask("Password: ") {|q| q.echo = "*"}
+      File.open("#{Dir.home}/.hubmaster", "w") {|f| f.write("#{@user}")}
+    end
 
-      return nil
+    @pass = ask("Password: ") {|q| q.echo = "*"}
+    return nil
   end
 
   def self.makeRequest(path, username = @user, password = @pass, server = "api.github.com")
@@ -24,7 +31,20 @@ module Github
 
   class Repos
     def self.view
-      Github.makeRequest("/user/repos")  
+      request = Github.makeRequest("/user/repos")  
+      JSON.parse(request).each do |repo|
+        puts "#{repo['name']} (#{repo['url']})"  
+        puts " - Language: #{repo['language']}"
+        created_at = repo['created_at'].split("T")
+        created_at_date = created_at[0].split("-")
+        created_at_date = "#{created_at_date[1]}/#{created_at_date[2]}/#{created_at_date[0]}" 
+        puts " - Created At: #{created_at[1]} on #{created_at_date}"
+        updated_at = repo['updated_at'].split("T")
+        updated_at_date = updated_at[0].split("-")
+        updated_at_date = "#{updated_at_date[1]}/#{updated_at_date[2]}/#{updated_at_date[0]}" 
+        puts " - Last Updated: #{updated_at[1]} on #{updated_at_date}"
+        puts ""
+      end
     end
   end
 end
