86579e8e1ddb3bcfb2836f5bec0dff9a8e300c18
Tommy Schaefer <me@tommyschaefer.net>
2012-10-29 13:06:44 -0500

    Made calls more natural

--- lib/hubmaster.rb
+++ lib/hubmaster.rb
@@ -4,30 +4,24 @@ require 'net/http'
 require 'net/https'
 
 module Github
-  class Connection
-    def initialize 
+  def self.connect
       print "Username: "
       @user = gets.chomp
       @pass = ask("Password: ") {|q| q.echo = "*"}
-    end
+  end
 
-    def makeRequest(path, username = @user, password = @pass, server = "api.github.com")
+  def self.makeRequest(path, username = @user, password = @pass, server = "api.github.com")
       http = Net::HTTP.new(server,443)
       req = Net::HTTP::Get.new(path)
       http.use_ssl = true
       req.basic_auth username, password
       response = http.request(req)
       return response.body
-    end
   end
 
   class Repos
-    def initialize(connection)
-      @connection = connection
-    end
-
-    def view
-      @connection.makeRequest("/user/repos")  
+    def self.view
+      Github.makeRequest("/user/repos")  
     end
   end
 end
