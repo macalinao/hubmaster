464fff32bd7abd13ecda9af169eb2a7395a8a143
Tommy Schaefer <me@tommyschaefer.net>
2012-11-07 20:00:18 -0600

    Fixed

--- lib/hubmaster.rb
+++ lib/hubmaster.rb
@@ -5,9 +5,6 @@ require 'net/http'
 require 'net/https'
 require 'stringio'
 require 'uri'
-require 'openssl'
-
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 require File.expand_path(File.join(File.dirname(__FILE__), "hubmaster", "base.rb"))
 require File.expand_path(File.join(File.dirname(__FILE__), "hubmaster", "repo.rb"))
